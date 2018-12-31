881fa1cbaabc8fd5084fc76d9851e0dd8453a3d8
Artem Antonenko <antonenko.artem@gmail.com>
2018-12-31 14:09:07 +0200

    [Create] presentation layer generator

--- src/generate-presentation.php
+++ src/generate-presentation.php
@@ -66,7 +66,7 @@ if (!file_exists($presentationServicesPath)){
     createFolder( $presentationServicesPath);
 }
 
-//addPresentationService($presentationServicesPath, $entity, $contract);
+addPresentationService($presentationServicesPath, $entity, $contract);
 
 $container = file_get_contents($presentationServicesContainerPath);
 
